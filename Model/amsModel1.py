7ad3d0de13bdfe241d20a31edf57f52d65822078
Harini S <131979231+HariniS1018@users.noreply.github.com>
2024-10-17 14:57:54 +0530

    Update amsModel1.py

--- Model/amsModel1.py
+++ Model/amsModel1.py
@@ -11,11 +11,11 @@ app.app_context().push()
 db = SQLAlchemy(app)
 
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = os.getenv(MAIL_PORT)
-app.config['MAIL_USE_TLS'] = os.getenv(MAIL_USE_TLS)
-app.config['MAIL_USERNAME'] = os.getenv(MAIL_USERNAME)
-app.config['MAIL_PASSWORD'] = os.getenv(MAIL_PASSWORD)
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv(MAIL_DEFAULT_SENDER)
+app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
 mail = Mail(app)
 
 if __name__ == '__main__':
